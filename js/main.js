1688fca691c22d8f80fb31fa0bd21c228cb8f53e
Jared Gray <jared@jaredgray.us>
2018-01-28 16:54:05 -0700

    Add smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
+
 $("#glide").glide({
   type: "carousel"
 });
