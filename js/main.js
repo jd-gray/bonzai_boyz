f7441e6d5926b97690f2189b4e14fe5f48a4a618
Jared Gray <jared@jaredgray.us>
2018-02-20 22:21:37 -0700

    Build website for soft launch

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-$(document).on('click', 'a[href^="#"]', function (event) {
-  event.preventDefault();
+(function (){
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
 
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 500);
-});
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
 
-$('.food-gallery').masonry({
-  // options...
-  itemSelector: '.grid-item',
-  fitWidth: true
-});
+  $('.food-gallery').masonry({
+    itemSelector: '.grid-item',
+    fitWidth: true
+  });
+})(jQuery);
